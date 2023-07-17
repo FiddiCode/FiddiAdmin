dc8e2fbf8562ea73816cdb02eaa4afcee754bd1e
fiddicode <coderfardeen@gmail.com>
2023-07-17 16:06:14 +0530

    modified:   client/src/components/Menu/Menu.jsx 	modified:   client/src/components/Menu/menu.scss 	new file:   client/src/data.js

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './menu.scss'
+import {menu} from "../../data"
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className='menu'>
+       {menu.map((item)=>(
+          <div className='item' key={item.id}>
+        <span className='title'>{item.title}</span>
+        {item.listItems.map((listItem)=>(
+          <Link to='/' className="listItem" key={listItem.id}> 
+        <img src={listItem.icon} alt=''/>
+        <span className='listItemTitle'>{listItem.title}</span>
+        </Link>))}
+      </div>))}
+    </div>
   )
 }
 
-export default Menu
+export default Menu;
