a21bbae869fb24c0b8f7773dd24e2e5427dad991
Fardeen Qureshi <coderfardeen@gmail.com>
2024-08-27 23:04:35 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Home from './pages/Home/Home.jsx ';
 import Products from './pages/Products/Products.jsx ';
 import Users from './pages/Users/Users.jsx ';
 import Navbar from './components/Navbar/Navbar.jsx';
-import Footer from './components/Footer/Footer.jsx';
+import Footer from './components/Footer/Footer.jsx ';
 import Menu from './components/Menu/Menu ';
 import Login from './pages/Login/Login ';
 import Product from './pages/Product/Product.jsx';
